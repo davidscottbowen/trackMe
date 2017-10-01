83bb7912ce37f84ba1dbc960c2ea23da19f13bfe
Scott <david.bowen@interapthq.com>
2017-10-01 19:15:31 -0400

    initialized project

---
+++ trackMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trackMe
+//
+//  Created by David Bowen on 10/1/17.
+//  Copyright © 2017 David Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
