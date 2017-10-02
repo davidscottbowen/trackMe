e2ce7aa343421ea5621f58ae3c34e9aa113b03cb
Scott <david.bowen@interapthq.com>
2017-10-02 17:39:48 -0400

    added data format in display

--- trackMe/HomeViewController.swift
+++ trackMe/HomeViewController.swift
@@ -12,25 +12,24 @@ import KeychainSwift
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var outputDisplay: UITextView!
     
     
     let keychain = KeychainSwift()
+    let formatter = DateFormatter()
+    var saved = ""
 
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var saved = keychain.get("my key")
-        if saved == nil {
-            print("No saved data")
-            saved = "No saved data"
-        } else {
-            print(String(describing: saved))
-        }
         
-        outputDisplay.text = String(describing: saved!)
+        outputDisplay.text = " "
         
+        getData()
 
+        
         // Do any additional setup after loading the view.
     }
 
@@ -38,6 +37,35 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getData() {
+        let checkForSave = keychain.get("my key")
+        
+        if checkForSave == nil {
+            saved = ""
+        } else {
+            saved = checkForSave!
+        }
+        formatter.dateFormat = "MM/dd/yyyy hh:mm:ss a"
+        let dateString = formatter.string(from: Date())
+        let latitudeString = "Latitude: 37.815446"
+        let longitudeString = "Longitude: -82.809549"
+        let currentString = dateString + "\n" + latitudeString + "\n" + longitudeString + "\n" + "\n"
+        let data = currentString + saved
+        
+        outputDisplay.text = String(describing: data)
+        
+        keychain.set(data, forKey: "my key")
+        
+    }
+    
+    
+    @IBAction func updateButtonTapped(_ sender: Any) {
+        outputDisplay.text = " "
+        getData()
+    }
+    
+    
     
 
     /*
